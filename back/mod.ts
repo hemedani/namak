3e3e320a02a7941a0eb190abda88daf1725a04e5
Syd Amir <hemedani@gmail.com>
2025-10-30 10:41:16 +0330

    🎉 Initialize Deno backend project with core configuration

---
+++ back/mod.ts
@@ -0,0 +1,45 @@
+import { lesan, MongoClient } from "@deps";
+import {
+	categories,
+	cities,
+	city_zones,
+	files,
+	provinces,
+	tags,
+	users,
+} from "@model";
+import { functionsSetup } from "./src/mod.ts";
+
+const MONGO_URI = Deno.env.get("MONGO_URI") || "mongodb://127.0.0.1:27017/";
+
+export const coreApp = lesan();
+const client = await new MongoClient(MONGO_URI).connect();
+const db = client.db("namak");
+coreApp.odm.setDb(db);
+
+export const user = users();
+export const file = files();
+export const province = provinces();
+export const city = cities();
+export const city_zone = city_zones();
+export const tag = tags();
+export const category = categories();
+
+export const { setAct, setService, getAtcsWithServices } = coreApp.acts;
+
+export const { selectStruct, getSchemas } = coreApp.schemas;
+
+functionsSetup();
+
+coreApp.runServer({
+	port: 1407,
+	typeGeneration: true,
+	playground: true,
+	staticPath: ["/uploads"],
+	cors: [
+		"http://localhost:3000",
+		"http://localhost:3006",
+		"http://194.5.192.166:3006",
+		"http://localhost:4006",
+	],
+});
